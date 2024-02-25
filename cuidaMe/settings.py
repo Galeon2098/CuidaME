d8d5e2a379550e3a846c0898dc4bf2d7db50b527
Juan Antonio Jiménez del Villar <73228070+JuanAntonioJimenezdelVillar@users.noreply.github.com>
2024-02-25 12:57:28 +0000

    feat: Pricing Plan view #54

--- cuidaMe/settings.py
+++ cuidaMe/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # my apps 
+    # my apps
     'main',
 ]
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- main/views.py
+++ main/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'main/home.html')
+
+def pricing_plan(request):
+    return render(request, 'main/pricingPlan.html')
