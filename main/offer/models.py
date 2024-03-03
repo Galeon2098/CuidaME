2cb49e6904d6a88776eec5d517074688878d1c4c
Cris <crigarher4@alum.us.es>
2024-03-03 18:14:13 +0100

    fix quality error with name timeSend

--- main/offer/models.py
+++ main/offer/models.py
@@ -45,7 +45,7 @@ class  ChatRequest(models.Model):
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='received_chat_requests')
     offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
     accepted = models.BooleanField(default=False)
-    timeSend = models.DateTimeField(auto_now_add=True)
+    time_Send = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.sender} to {self.receiver}'
