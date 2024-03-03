05ed31964ed52e215ffd13021fe82f1237a32169
Fraprapra1 <61785659+Galeon2098@users.noreply.github.com>
2024-03-03 20:38:03 +0100

    Update views.py
    eliminado doble importación de "redirect"

--- main/offer/views.py
+++ main/offer/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Q  
-from django.shortcuts import render, redirect, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from .models import Offer
