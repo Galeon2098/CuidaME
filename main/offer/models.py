b366afce2f1f7abb92ab44fec341f64871770f95
jualeomad <jualeomad@alum.us.es>
2024-03-04 20:34:42 +0100

    fix: codacy

--- main/offer/models.py
+++ main/offer/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
 
 class Offer(models.Model):
 
