fbfd98c3073e5a604d4877f233daf496654b7f22
Cris <crigarher4@alum.us.es>
2024-03-04 22:09:35 +0100

    fix:errores entrega sprint 1

--- cuidaMe/urls.py
+++ cuidaMe/urls.py
@@ -39,5 +39,5 @@ urlpatterns = [path('admin/', admin.site.urls),
     path('product_page', views.product_page, name='product_page'), #Con fines de testeo
     path('payment_successful', views.payment_successful, name='payment_successful'),
     path('payment_cancelled', views.payment_cancelled, name='payment_cancelled'),
-    path('offer/', include('main.offer.urls', namespace='offer'))
+
 ]
