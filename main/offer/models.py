3a50a3635ddb6ecf24ebd4f6e23bc2f89eca5ff4
Lucperrom <luciaperezromero07@gmail.com>
2024-03-08 22:36:04 +0100

    fix: fallos offers #146

--- main/offer/models.py
+++ main/offer/models.py
@@ -22,11 +22,11 @@ class Offer(models.Model):
         ('OT', 'OTROS')
     )
 
-    title = models.CharField(max_length=200, verbose_name='Título')
+    title = models.CharField(max_length=75, verbose_name='Título')
     offer_type= models.CharField(max_length=255, verbose_name='Tipo de oferta', choices=TYPE_CHOICES, default='OT')
     client = models.CharField(max_length=255, verbose_name='Tipo de cliente', choices=CLIENT_CHOICES, default='OT')
     description = models.TextField(blank=True)
-    price_per_hour = models.DecimalField(max_digits=10,decimal_places=2)
+    price_per_hour = models.DecimalField(max_digits=10,decimal_places=2, validators=[MinValueValidator(1.00), MaxValueValidator(100.00)])
     city = models.CharField(max_length=200, verbose_name='Ciudad')
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
