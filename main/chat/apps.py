70773ce0a72952dca35f1be4629f3766b38178f1
rafgargal <rafgargal@alum.us.es>
2024-03-09 22:22:24 +0100

    fix: added important import

--- main/chat/apps.py
+++ main/chat/apps.py
@@ -4,4 +4,7 @@ from django.apps import AppConfig
 class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main.chat'
-        
+
+    # si se borra este import, no funcioa    
+    def ready(self):
+        import main.chat.signals
