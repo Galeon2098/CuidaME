afb35db87b3774a93518ab50cf4bb5cdc5a98ce1
mohanadabulatifa <mohanad.abulatifa@gmail.com>
2024-03-15 15:44:16 +0100

    fix:sonarcloud

--- main/offer/views.py
+++ main/offer/views.py
@@ -154,7 +154,7 @@ def editar_oferta_admin(request, offer_id):
     return render(request, 'offers/editar_oferta_admin.html', {'form': form, 'oferta': oferta})
 @staff_member_required
 @user_passes_test(lambda u: u.is_superuser)
-@require_http_methods(["GET", "POST"])
+@require_http_methods(["POST"])
 def eliminar_oferta_admin(request, offer_id):
     oferta = get_object_or_404(Offer, pk=offer_id)
     if request.method == 'POST':
