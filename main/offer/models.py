20d4ddce18ebd0824d1c1f47c25d7f0483af6ef9
mohanadabulatifa <mohanad.abulatifa@gmail.com>
2024-03-16 18:30:02 +0100

    fix:arreglar ultimo error estitico

--- main/offer/models.py
+++ main/offer/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 from main.models import Cuidador
-from .choices import POB_CHOICES
 
 class Offer(models.Model):
 
