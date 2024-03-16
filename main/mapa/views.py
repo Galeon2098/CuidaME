c68c26d97131cc4e330adfe4c04b7be52474dd68
Juan Antonio Jiménez del Villar <73228070+JuanAntonioJimenezdelVillar@users.noreply.github.com>
2024-03-16 17:02:21 +0000

    feat: Redirigir a detalles de la oferta #167

--- main/mapa/views.py
+++ main/mapa/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse
 from main.offer.models import Offer
 import folium
 from folium.plugins import FastMarkerCluster
+from folium import Marker, Popup, IFrame
 
 def mapaHome(request):
     offers = Offer.objects.all()
@@ -12,7 +14,13 @@ def mapaHome(request):
     longitudes = [offer.lng for offer in offers]
     popups = [offer.title for offer in offers]
 
-    FastMarkerCluster(data=list(zip(latitudes, longitudes, popups))).add_to(initialMap)
+    for lat, lng, popup, offer in zip(latitudes, longitudes, popups, offers):
+        offer_url = request.build_absolute_uri(reverse('offer:detail', args=[offer.id]))
+        popup_html = f'<a href="{offer_url}" target="_blank">{popup}</a><br>Title: {offer.title}<br>Description: {offer.description}<br>Type: {offer.offer_type}<br>Price: {offer.price_per_hour}'
+        iframe = IFrame(popup_html, width=200, height=100)
+        popup = Popup(iframe, max_width=200)
+        marker = Marker([lat, lng], popup=popup)
+        marker.add_to(initialMap)
 
     context = {'map':initialMap._repr_html_(), 'offers':offers}
     return render(request, 'mapa/mapa.html', context)
