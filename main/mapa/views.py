e45a059b5a05f5d09508bfddff08f2ed6143fadd
rafgargal <rafgargal@alum.us.es>
2024-03-17 13:09:43 +0100

    feat: agrupación de marcadores + mejora visual #167

--- main/mapa/views.py
+++ main/mapa/views.py
@@ -8,19 +8,18 @@ from folium import Marker, Popup, IFrame
 def mapaHome(request):
     offers = Offer.objects.all()
 
-    initialMap = folium.Map(location=[37.3887735,-5.9835773], zoom_start=13) #Sevilla
+    initialMap = folium.Map(location=[37.3887735, -5.9835773], zoom_start=13)  # Sevilla
 
-    latitudes = [offer.lat for offer in offers]
-    longitudes = [offer.lng for offer in offers]
-    popups = [offer.title for offer in offers]
+    marker_cluster = FastMarkerCluster([], name="Offers")
+    marker_cluster.add_to(initialMap)
 
-    for lat, lng, popup, offer in zip(latitudes, longitudes, popups, offers):
+    for offer in offers:
         offer_url = request.build_absolute_uri(reverse('offer:detail', args=[offer.id]))
-        popup_html = f'<a href="{offer_url}" target="_blank">{popup}</a><br>Title: {offer.title}<br>Description: {offer.description}<br>Type: {offer.offer_type}<br>Price: {offer.price_per_hour}'
-        iframe = IFrame(popup_html, width=200, height=100)
+        type_name = dict(Offer.TYPE_CHOICES)[offer.offer_type]
+        popup_html = f'<h3><a href="{offer_url}" target="_blank">{offer.title}</a></h3><strong>Descripción:</strong> {offer.description}<br><strong>Tipo:</strong> {type_name}<br><strong>Precio:</strong> {offer.price_per_hour}'
+        iframe = IFrame(popup_html, width=200, height=120)
         popup = Popup(iframe, max_width=200)
-        marker = Marker([lat, lng], popup=popup)
-        marker.add_to(initialMap)
+        Marker([offer.lat, offer.lng], popup=popup).add_to(marker_cluster)
 
-    context = {'map':initialMap._repr_html_(), 'offers':offers}
+    context = {'map': initialMap._repr_html_(), 'offers': offers}
     return render(request, 'mapa/mapa.html', context)
