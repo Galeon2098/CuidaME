097c75234c76fddccd151ba5b96ae38d0a3d1ad4
Cris <crigarher4@alum.us.es>
2024-03-18 13:48:49 +0100

    fix:codacy quality error por un espacio :')

--- main/foro/forms.py
+++ main/foro/forms.py
@@ -3,7 +3,6 @@ from django import forms
 from .models import Thread, Comment
 
 class ThreadForm(forms.ModelForm):
-   
     class Meta:
         model = Thread
         fields = ['title','description']
