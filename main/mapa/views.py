d152bf4cc1cd40c83ef1a21b3c57fbc62ef87d41
Juan Antonio Jiménez del Villar <73228070+JuanAntonioJimenezdelVillar@users.noreply.github.com>
2024-03-22 18:14:07 +0000

    fix: Arreglados errores de codacy #167

--- main/mapa/views.py
+++ main/mapa/views.py
@@ -18,18 +18,18 @@ def mapaHome(request):
             initialMap = folium.Map(location=[37.3887735, -5.9835773], zoom_start=13)
     else:
         initialMap = folium.Map(location=[37.3887735, -5.9835773], zoom_start=13)  # Sevilla
-    
+
     marker_cluster = FastMarkerCluster([], name="Offers")
     marker_cluster.add_to(initialMap)
-    
+
     offers = Offer.objects.all()
-    
+
     for offer in offers:
         offer_url = request.build_absolute_uri(reverse('offer:detail', args=[offer.id]))
         type_name = dict(Offer.TYPE_CHOICES)[offer.offer_type]
         popup_html = f'<h3><a href="{offer_url}" target="_blank">{offer.title}</a></h3><strong>Descripción:</strong> {offer.description}<br><strong>Tipo:</strong> {type_name}<br><strong>Precio:</strong> {offer.price_per_hour}'
         popup = Popup(popup_html, max_width=200)
         Marker([offer.lat, offer.lng], popup=popup).add_to(marker_cluster)
-    
+
     context = {'map': initialMap._repr_html_(), 'offers': offers}
     return render(request, 'mapa/mapa.html', context)
